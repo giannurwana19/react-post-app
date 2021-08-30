2e4658d27e92fd81c36bfd55e07181cb63cc18b1
giannurwana19 <giannurwana19@gmail.com>
2021-08-30 18:45:20 +0700

    setup search query

--- src/App.js
+++ src/App.js
@@ -1,17 +1,32 @@
 import { Container } from '@material-ui/core';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Redirect,
+  Route,
+  Switch,
+} from 'react-router-dom';
 import Auth from './components/Auth';
-import Home from './components/Home';
+import Home from './pages/Home';
 import Navbar from './components/Navbar';
+import PostDetail from './pages/PostDetail';
 
 function App() {
+  const user = JSON.parse(localStorage.getItem('profile'));
+
   return (
     <Router>
-      <Container maxWidth="lg">
+      <Container maxWidth="xl">
         <Navbar />
         <Switch>
-          <Route exact path="/" component={Home} />
-          <Route path="/auth" component={Auth} />
+          <Route exact path="/" component={() => <Redirect to="/posts" />} />
+          <Route exact path="/posts" component={Home} />
+          <Route exact path="/posts/search" component={Home} />
+          <Route exact path="/posts/:id" component={PostDetail} />
+          <Route
+            exact
+            path="/auth"
+            component={() => (!user ? <Auth /> : <Redirect to="/posts" />)}
+          />
         </Switch>
       </Container>
     </Router>
